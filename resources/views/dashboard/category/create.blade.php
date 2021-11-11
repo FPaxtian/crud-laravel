b235e68f4f0e535c71d68ddf187b20d73d29c2aa
Francisco Paxtian <fizquierdo@quaxar.com>
2021-11-10 20:18:18 -0600

    success install boostrap

--- resources/views/dashboard/category/create.blade.php
+++ resources/views/dashboard/category/create.blade.php
@@ -1,8 +1,25 @@
-<form action="{{ route('category.store') }}" method="post">
-    @csrf
-    <div class="form-group">
-        <label for="name_category">Nombre de la categoria</label>
-        <input type="text" name="name_category" id="name_category" class="form-control">
-    </div>
-    <input type="submit" value="Registrar" class="btn btn-primary">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('css/app.css') }}"></script>
+</head>
+
+<body class="container">
+
+    <form action="{{ route('category.store') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="name_category">Nombre de la categoria</label>
+            <input type="text" name="name_category" id="name_category" class="form-control">
+        </div>
+        <input type="submit" value="Registrar" class="btn btn-primary">
+    </form>
+</body>
+
+</html>
